803a7509522b423fed18876484b7805a27a4ae45
Kim Falk <kfj@sitecore.net>
2016-03-07 21:42:16 +0100

    last changes for updated chapter 3

--- prs/evidenceCollector/views.py
+++ prs/evidenceCollector/views.py
@@ -9,8 +9,9 @@ def index(request, contentid, event):
 
     sessionId = request.GET.get('sessionid', 0)
     userid = request.GET.get('userid', 0)
-    date =  request.GET.get('date', datetime.datetime.now().isoformat())
+    date = request.GET.get('date', datetime.datetime.now().isoformat())
     visitCount = request.GET.get('visitCount', 0)
+
     l = Log(created=date, \
     	    user_id = userid, \
     	    content_id = contentid, \
